a8d7d79ffe8c67a0bfb0e32c3bdea07f093ba059
Martijn Groenier <martijn.groenier@hva.nl>
2018-06-02 13:24:06 +0200

    Got rid of duplicate code by creating a new class and letting the two other classes extend that one.

--- src/protocol/http/org/apache/jmeter/protocol/http/config/gui/HttpDefaultsGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/config/gui/HttpDefaultsGui.java
@@ -39,6 +39,7 @@ import org.apache.jmeter.config.gui.AbstractConfigGui;
 import org.apache.jmeter.gui.GUIMenuSortOrder;
 import org.apache.jmeter.gui.util.HorizontalPanel;
 import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.protocol.http.HttpGui;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerFactory;
 import org.apache.jmeter.testelement.AbstractTestElement;
@@ -53,28 +54,7 @@ import org.apache.jorphan.gui.JLabeledTextField;
  * GUI for Http Request defaults
  */
 @GUIMenuSortOrder(5)
-public class HttpDefaultsGui extends AbstractConfigGui {
-
-    private static final long serialVersionUID = 241L;
-
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font");
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8));
-
-    private UrlConfigGui urlConfigGui;
-    private JCheckBox retrieveEmbeddedResources;
-    private JCheckBox concurrentDwn;
-    private JTextField concurrentPool;
-    private JCheckBox useMD5;
-    private JLabeledTextField embeddedRE; // regular expression used to match against embedded resource URLs
-    private JTextField sourceIpAddr; // does not apply to Java implementation
-    private JComboBox<String> sourceIpType = new JComboBox<>(HTTPSamplerBase.getSourceTypeList());
-    private JTextField proxyHost;
-    private JTextField proxyPort;
-    private JTextField proxyUser;
-    private JPasswordField proxyPass;
-    private JComboBox<String> httpImplementation = new JComboBox<>(HTTPSamplerFactory.getImplementations());
-    private JTextField connectTimeOut;
-    private JTextField responseTimeOut;
+public class HttpDefaultsGui extends HttpGui {
 
     public HttpDefaultsGui() {
         super();
@@ -239,210 +219,10 @@ public class HttpDefaultsGui extends AbstractConfigGui {
         add(tabbedPane, BorderLayout.CENTER);        
         add(emptyPanel, BorderLayout.SOUTH);
     }
-    
-    private JPanel getTimeOutPanel() {
-        JPanel timeOut = new HorizontalPanel();
-        timeOut.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_server_timeout_title"))); // $NON-NLS-1$
-        final JPanel connPanel = getConnectTimeOutPanel();
-        final JPanel reqPanel = getResponseTimeOutPanel();
-        timeOut.add(connPanel);
-        timeOut.add(reqPanel);
-        return timeOut;
-    }
-    
-    private JPanel getConnectTimeOutPanel() {
-        connectTimeOut = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_connect")); // $NON-NLS-1$
-        label.setLabelFor(connectTimeOut);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(connectTimeOut, BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    private JPanel getResponseTimeOutPanel() {
-        responseTimeOut = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_response")); // $NON-NLS-1$
-        label.setLabelFor(responseTimeOut);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(responseTimeOut, BorderLayout.CENTER);
-
-        return panel;
-    }
-    
-    protected JPanel createEmbeddedRsrcPanel() {
-        // retrieve Embedded resources
-        retrieveEmbeddedResources = new JCheckBox(JMeterUtils.getResString("web_testing_retrieve_images")); // $NON-NLS-1$
-        // add a listener to activate or not concurrent dwn.
-        retrieveEmbeddedResources.addItemListener(e -> {
-            if (e.getStateChange() == ItemEvent.SELECTED) { enableConcurrentDwn(true); }
-            else { enableConcurrentDwn(false); }
-        });
-        // Download concurrent resources
-        concurrentDwn = new JCheckBox(JMeterUtils.getResString("web_testing_concurrent_download")); // $NON-NLS-1$
-        concurrentDwn.addItemListener(e -> {
-            if (retrieveEmbeddedResources.isSelected() && e.getStateChange() == ItemEvent.SELECTED) { concurrentPool.setEnabled(true); }
-            else { concurrentPool.setEnabled(false); }
-        });
-        concurrentPool = new JTextField(2); // 2 columns size
-        concurrentPool.setMinimumSize(new Dimension(10, (int) concurrentPool.getPreferredSize().getHeight()));
-        concurrentPool.setMaximumSize(new Dimension(30, (int) concurrentPool.getPreferredSize().getHeight()));
-
-        final JPanel embeddedRsrcPanel = new HorizontalPanel();
-        embeddedRsrcPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("web_testing_retrieve_title"))); // $NON-NLS-1$
-        embeddedRsrcPanel.add(retrieveEmbeddedResources);
-        embeddedRsrcPanel.add(concurrentDwn);
-        embeddedRsrcPanel.add(concurrentPool);
-        
-        // Embedded URL match regex
-        embeddedRE = new JLabeledTextField(JMeterUtils.getResString("web_testing_embedded_url_pattern"),20); // $NON-NLS-1$
-        embeddedRsrcPanel.add(embeddedRE); 
-        
-        return embeddedRsrcPanel;
-    }
-    
-    protected JPanel createSourceAddrPanel() {
-        final JPanel sourceAddrPanel = new HorizontalPanel();
-        sourceAddrPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("web_testing_source_ip"))); // $NON-NLS-1$
-
-        sourceIpType.setSelectedIndex(HTTPSamplerBase.SourceType.HOSTNAME.ordinal()); //default: IP/Hostname
-        sourceAddrPanel.add(sourceIpType);
-
-        sourceIpAddr = new JTextField();
-        sourceAddrPanel.add(sourceIpAddr);
-        return sourceAddrPanel;
-    }
-    
-    protected JPanel createOptionalTasksPanel() {
-        // OPTIONAL TASKS
-        final JPanel checkBoxPanel = new VerticalPanel();
-        checkBoxPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("optional_tasks"))); // $NON-NLS-1$
-
-        // Use MD5
-        useMD5 = new JCheckBox(JMeterUtils.getResString("response_save_as_md5")); // $NON-NLS-1$
-
-        checkBoxPanel.add(useMD5);
-
-        return checkBoxPanel;
-    }
 
     @Override
     public Dimension getPreferredSize() {
         return getMinimumSize();
     }
-    
-    private void enableConcurrentDwn(final boolean enable) {
-        if (enable) {
-            concurrentDwn.setEnabled(true);
-            embeddedRE.setEnabled(true);
-            if (concurrentDwn.isSelected()) {
-                concurrentPool.setEnabled(true);
-            }
-        } else {
-            concurrentDwn.setEnabled(false);
-            concurrentPool.setEnabled(false);
-            embeddedRE.setEnabled(false);
-        }
-    }
-    
-    /**
-     * Create a panel containing the implementation details
-     *
-     * @return the panel
-     */
-    protected final JPanel getImplementationPanel(){
-        JPanel implPanel = new HorizontalPanel();
-        implPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_server_client"))); // $NON-NLS-1$
-        implPanel.add(new JLabel(JMeterUtils.getResString("http_implementation"))); // $NON-NLS-1$
-        httpImplementation.addItem("");// $NON-NLS-1$
-        implPanel.add(httpImplementation);
-        return implPanel;
-    }
 
-    /**
-     * Create a panel containing the proxy server details
-     *
-     * @return the panel
-     */
-    protected final JPanel getProxyServerPanel(){
-        JPanel proxyServer = new HorizontalPanel();
-        proxyServer.add(getProxyHostPanel(), BorderLayout.CENTER);
-        proxyServer.add(getProxyPortPanel(), BorderLayout.EAST);
-
-        JPanel proxyLogin = new HorizontalPanel();
-        proxyLogin.add(getProxyUserPanel());
-        proxyLogin.add(getProxyPassPanel());
-
-        JPanel proxyServerPanel = new HorizontalPanel();
-        proxyServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_proxy_server_title"))); // $NON-NLS-1$
-        proxyServerPanel.add(proxyServer);
-        proxyServerPanel.add(proxyLogin);
-
-        return proxyServerPanel;
-    }
-
-    private JPanel getProxyHostPanel() {
-        proxyHost = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_domain")); // $NON-NLS-1$
-        label.setLabelFor(proxyHost);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyHost, BorderLayout.CENTER);
-        return panel;
-    }
-    
-    private JPanel getProxyPortPanel() {
-        proxyPort = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_port")); // $NON-NLS-1$
-        label.setLabelFor(proxyPort);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyPort, BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    private JPanel getProxyUserPanel() {
-        proxyUser = new JTextField(5);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("username")); // $NON-NLS-1$
-        label.setLabelFor(proxyUser);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyUser, BorderLayout.CENTER);
-        return panel;
-    }
-
-    private JPanel getProxyPassPanel() {
-        proxyPass = new JPasswordField(5);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("password")); // $NON-NLS-1$
-        label.setLabelFor(proxyPass);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyPass, BorderLayout.CENTER);
-        return panel;
-    }
 }
--- src/protocol/http/org/apache/jmeter/protocol/http/control/gui/HttpTestSampleGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/gui/HttpTestSampleGui.java
@@ -37,6 +37,7 @@ import javax.swing.UIManager;
 import org.apache.jmeter.gui.GUIMenuSortOrder;
 import org.apache.jmeter.gui.util.HorizontalPanel;
 import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.protocol.http.HttpGui;
 import org.apache.jmeter.protocol.http.config.gui.UrlConfigGui;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerFactory;
@@ -50,28 +51,7 @@ import org.apache.jorphan.gui.JLabeledTextField;
  * HTTP Sampler GUI
  */
 @GUIMenuSortOrder(1)
-public class HttpTestSampleGui extends AbstractSamplerGui {
-    
-    private static final long serialVersionUID = 241L;
-    
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font");
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8));
-    
-    private UrlConfigGui urlConfigGui;
-    private JCheckBox retrieveEmbeddedResources;
-    private JCheckBox concurrentDwn;
-    private JTextField concurrentPool;
-    private JCheckBox useMD5;
-    private JLabeledTextField embeddedRE; // regular expression used to match against embedded resource URLs
-    private JTextField sourceIpAddr; // does not apply to Java implementation
-    private JComboBox<String> sourceIpType = new JComboBox<>(HTTPSamplerBase.getSourceTypeList());
-    private JTextField proxyHost;
-    private JTextField proxyPort;
-    private JTextField proxyUser;
-    private JPasswordField proxyPass;
-    private JComboBox<String> httpImplementation = new JComboBox<>(HTTPSamplerFactory.getImplementations());
-    private JTextField connectTimeOut;
-    private JTextField responseTimeOut;
+public class HttpTestSampleGui extends HttpGui {
 
     private final boolean isAJP;
 
@@ -195,117 +175,6 @@ public class HttpTestSampleGui extends AbstractSamplerGui {
         add(splitPane);
     }
 
-    private JPanel getTimeOutPanel() {
-        JPanel timeOut = new HorizontalPanel();
-        timeOut.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_server_timeout_title"))); // $NON-NLS-1$
-        final JPanel connPanel = getConnectTimeOutPanel();
-        final JPanel reqPanel = getResponseTimeOutPanel();
-        timeOut.add(connPanel);
-        timeOut.add(reqPanel);
-        return timeOut;
-    }
-    
-    private JPanel getConnectTimeOutPanel() {
-        connectTimeOut = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_connect")); // $NON-NLS-1$
-        label.setLabelFor(connectTimeOut);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(connectTimeOut, BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    private JPanel getResponseTimeOutPanel() {
-        responseTimeOut = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_response")); // $NON-NLS-1$
-        label.setLabelFor(responseTimeOut);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(responseTimeOut, BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    protected JPanel createEmbeddedRsrcPanel() {
-        // retrieve Embedded resources
-        retrieveEmbeddedResources = new JCheckBox(JMeterUtils.getResString("web_testing_retrieve_images")); // $NON-NLS-1$
-        // add a listener to activate or not concurrent dwn.
-        retrieveEmbeddedResources.addItemListener(e -> {
-            if (e.getStateChange() == ItemEvent.SELECTED) { enableConcurrentDwn(true); }
-            else { enableConcurrentDwn(false); }
-        });
-        // Download concurrent resources
-        concurrentDwn = new JCheckBox(JMeterUtils.getResString("web_testing_concurrent_download")); // $NON-NLS-1$
-        concurrentDwn.addItemListener(e -> {
-            if (retrieveEmbeddedResources.isSelected() && e.getStateChange() == ItemEvent.SELECTED) { concurrentPool.setEnabled(true); }
-            else { concurrentPool.setEnabled(false); }
-        });
-        concurrentPool = new JTextField(2); // 2 column size
-        concurrentPool.setMinimumSize(new Dimension(10, (int) concurrentPool.getPreferredSize().getHeight()));
-        concurrentPool.setMaximumSize(new Dimension(30, (int) concurrentPool.getPreferredSize().getHeight()));
-
-        final JPanel embeddedRsrcPanel = new HorizontalPanel();
-        embeddedRsrcPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("web_testing_retrieve_title"))); // $NON-NLS-1$
-        embeddedRsrcPanel.add(retrieveEmbeddedResources);
-        embeddedRsrcPanel.add(concurrentDwn);
-        embeddedRsrcPanel.add(concurrentPool);
-
-        // Embedded URL match regex
-        embeddedRE = new JLabeledTextField(JMeterUtils.getResString("web_testing_embedded_url_pattern"),20); // $NON-NLS-1$
-        embeddedRsrcPanel.add(embeddedRE);
-        
-        return embeddedRsrcPanel;
-    }
-
-    /**
-     * Create a panel containing the implementation details
-     *
-     * @return the panel
-     */
-    protected final JPanel getImplementationPanel(){
-        JPanel implPanel = new HorizontalPanel();
-        implPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_server_client"))); // $NON-NLS-1$
-        implPanel.add(new JLabel(JMeterUtils.getResString("http_implementation"))); // $NON-NLS-1$
-        httpImplementation.addItem("");// $NON-NLS-1$
-        implPanel.add(httpImplementation);
-        return implPanel;
-    }
-    
-    protected JPanel createOptionalTasksPanel() {
-        // OPTIONAL TASKS
-        final JPanel checkBoxPanel = new VerticalPanel();
-        checkBoxPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("optional_tasks"))); // $NON-NLS-1$
-
-        // Use MD5
-        useMD5 = new JCheckBox(JMeterUtils.getResString("response_save_as_md5")); // $NON-NLS-1$
-        checkBoxPanel.add(useMD5);
-
-        return checkBoxPanel;
-    }
-    
-    protected JPanel createSourceAddrPanel() {
-        final JPanel sourceAddrPanel = new HorizontalPanel();
-        sourceAddrPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), JMeterUtils
-                .getResString("web_testing_source_ip"))); // $NON-NLS-1$
-
-        // Add a new field source ip address (for HC implementations only)
-        sourceIpType.setSelectedIndex(HTTPSamplerBase.SourceType.HOSTNAME.ordinal()); //default: IP/Hostname
-        sourceAddrPanel.add(sourceIpType);
-
-        sourceIpAddr = new JTextField();
-        sourceAddrPanel.add(sourceIpAddr);
-        return sourceAddrPanel;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -339,95 +208,5 @@ public class HttpTestSampleGui extends AbstractSamplerGui {
             responseTimeOut.setText(""); // $NON-NLS-1$
         }
     }
-    
-    private void enableConcurrentDwn(boolean enable) {
-        if (enable) {
-            concurrentDwn.setEnabled(true);
-            embeddedRE.setEnabled(true);
-            if (concurrentDwn.isSelected()) {
-                concurrentPool.setEnabled(true);
-            }
-        } else {
-            concurrentDwn.setEnabled(false);
-            concurrentPool.setEnabled(false);
-            embeddedRE.setEnabled(false);
-        }
-    }
-    
 
-    /**
-     * Create a panel containing the proxy server details
-     *
-     * @return the panel
-     */
-    protected final JPanel getProxyServerPanel(){
-        JPanel proxyServer = new HorizontalPanel();
-        proxyServer.add(getProxyHostPanel(), BorderLayout.CENTER);
-        proxyServer.add(getProxyPortPanel(), BorderLayout.EAST);
-
-        JPanel proxyLogin = new HorizontalPanel();
-        proxyLogin.add(getProxyUserPanel());
-        proxyLogin.add(getProxyPassPanel());
-
-        JPanel proxyServerPanel = new HorizontalPanel();
-        proxyServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_proxy_server_title"))); // $NON-NLS-1$
-        proxyServerPanel.add(proxyServer);
-        proxyServerPanel.add(proxyLogin);
-
-        return proxyServerPanel;
-    }
-
-    private JPanel getProxyHostPanel() {
-        proxyHost = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_domain")); // $NON-NLS-1$
-        label.setLabelFor(proxyHost);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyHost, BorderLayout.CENTER);
-        return panel;
-    }
-    
-    private JPanel getProxyPortPanel() {
-        proxyPort = new JTextField(10);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("web_server_port")); // $NON-NLS-1$
-        label.setLabelFor(proxyPort);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyPort, BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    private JPanel getProxyUserPanel() {
-        proxyUser = new JTextField(5);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("username")); // $NON-NLS-1$
-        label.setLabelFor(proxyUser);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyUser, BorderLayout.CENTER);
-        return panel;
-    }
-
-    private JPanel getProxyPassPanel() {
-        proxyPass = new JPasswordField(5);
-
-        JLabel label = new JLabel(JMeterUtils.getResString("password")); // $NON-NLS-1$
-        label.setLabelFor(proxyPass);
-        label.setFont(FONT_SMALL);
-
-        JPanel panel = new JPanel(new BorderLayout(5, 0));
-        panel.add(label, BorderLayout.WEST);
-        panel.add(proxyPass, BorderLayout.CENTER);
-        return panel;
-    }
 }
